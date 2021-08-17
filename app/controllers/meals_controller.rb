0ce94416a22428107269ad15a4c5e8d067c7af01
adrien leclercq <leclercq.adrien96@gmail.com>
2021-08-17 14:50:22 +0200

    Add dotenv - Protect my secret data in .env file

---
+++ app/controllers/meals_controller.rb
@@ -0,0 +1,25 @@
+class MealsController < ApplicationController
+  def index
+    @meals = Meal.all
+  end
+
+  def new
+    @meal = Meal.new
+  end
+
+  def create
+    @meal = Meal.new(meal_params)
+    @meal.user_id = current_user.id
+    if @meal.save
+      redirect_to meals_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def meal_params
+    params.require(:meal).permit(:title, :category, :description, :price)
+  end
+end
