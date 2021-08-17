75a9edb14609b463acb28eb9b847acd58ff1d7d0
adrien leclercq <leclercq.adrien96@gmail.com>
2021-08-17 15:43:11 +0200

    Routs meal + add photo user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :meals
   has_many :bookings
   has_many :bookings, through: :meals
+  has_one_attached :photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
