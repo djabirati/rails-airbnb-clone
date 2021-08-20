4b378e5fc37243113cd13637e0b21e8f2ebec10a
kevin decker <kdecker.webmaster@gmail.com>
2021-08-20 11:15:11 +0200

    correction routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :meals, only: %i[index new create edit update] do
+  resources :meals, only: %i[index new create edit update show] do
     resources :bookings, only: [:new, :create, :update]
   end
   resources :bookings, only: :destroy
